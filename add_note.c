bf4000358cc416889498f45ea1b95d319844cd5e
dammysaina <dammyadediran94@gmail.com>
2022-07-14 18:03:15 -0700

    addqueue

--- add_note.c
+++ add_note.c
@@ -31,7 +31,7 @@ void addqueue(stack_t **head, int n)
 {
 	stack_t *new_node, *temp;
 	temp = *head;
-	new_node = malloc(sizeof(stack_t))
+	new_node = malloc(sizeof(stack_t));
 		if (new_node == NULL)
 		{
 			printf("Error\n");
